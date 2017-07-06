98b4093fb0772c512b1d9d160096b2d8a22ea1f1
Dushyant Joshi <dushyantjoshia@gmail.com>
2017-07-06 19:27:58 +0530

    second commit

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -33,7 +33,7 @@ exports.book_list = function(req, res, next) {
   Book.find({}, 'title author ')
     .populate('author')
     .exec(function (err, list_books) {
-      if (err) { return next(err); }      
+      if (err) { return next(err); }
       //Successful, so render
       res.render('book_list', { title: 'Book List', book_list: list_books });
     });
